b9e2b6d685c4c72d82c791d8512b45e9293e036b
Christoph Lackner <clackner@gridprotectionalliance.org>
2021-06-11 17:36:03 -0400

    Fixed Logic for OpenHistoiran Datasource

--- Source/Library/Adapt.DataSources/OpenHistorian.cs
+++ Source/Library/Adapt.DataSources/OpenHistorian.cs
@@ -24,6 +24,7 @@
 
 using Adapt.Models;
 using Gemstone;
+using Gemstone.Collections.CollectionExtensions;
 using GemstoneCommon;
 using GemstonePhasorProtocolls;
 using Microsoft.Extensions.Configuration;
@@ -38,6 +39,9 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -53,11 +57,92 @@ namespace Adapt.DataSources
     [Description("OpenHistorian: Imports Phasor data from an OpenHistorian.")]
     public class OpenHistorian : IDataSource
     {
+        #region [ Classes ]
+        [Serializable]
+
         
+        public class HistorianAggregatePoint
+        {
+            public string Tag { get; set; }
+            public string Timestamp { get; set; }
+            public double Minimum { get; set; }
+            public double Average { get; set; }
+            public double Maximum { get; set; }
+            public ulong QualityFlags { get; set; }
+
+        }
+
+        /// <summary>
+        /// Body of post queries to Historian
+        /// </summary>
+        public class Post
+        {
+            /// <summary>
+            /// The acronym of the historian instance to query from
+            /// </summary>
+            public string Instance { get; set; }
+            /// <summary>
+            /// How the result is aggregated 1s,1m,1d,1w
+            /// </summary>
+            public string Aggregate { get; set; }
+            /// <summary>
+            /// List of device names to include in query results
+            /// </summary>
+            public string[] Devices { get; set; }
+            /// <summary>
+            /// List of Phases to include in query results
+            /// </summary>
+            public string[] Phases { get; set; }
+            /// <summary>
+            /// List of SignalTypes to include in query results
+            /// </summary>
+            public string[] Types { get; set; }
+            /// <summary>
+            /// Bit flag of valid Hours of the day to include in query results
+            /// </summary>
+            public ulong Hours { get; set; }
+            /// <summary>
+            /// Bit flag of valid Days of the week to include in query results
+            /// </summary>
+            public ulong Days { get; set; }
+            /// <summary>
+            /// Bit flag of valid Weeks of the year to include in query results
+            /// </summary>
+            public ulong Weeks { get; set; }
+            /// <summary>
+            /// Bit flag of valid Months of the year to include in query results
+            /// </summary>
+            public ulong Months { get; set; }
+            /// <summary>
+            /// Start time of query results
+            /// </summary>
+            public DateTime StartTime { get; set; }
+            /// <summary>
+            /// End time of query results
+            /// </summary>
+            public DateTime EndTime { get; set; }
+
+            ///<summary>
+            ///  List of Point IDs to include in query results
+            ///  Note that this will override <see cref="Types"/>, <see cref="Phases"/> and <see cref="Devices"/>
+            ///</summary>
+            public string[]? Tags { get; set; }
+
+        }
+
+        #endregion
+
         #region [ Members ]
+
         private OpenHistorianSettings m_settings;
         private List<AdaptDevice> m_Devices = null;
         private List<AdaptSignal> m_Signals = null;
+
+        const ulong ValidHours = 16777215; // Math.Pow(2, 24) - 1
+        const ulong ValidDays = 127; //  (int)(Math.Pow(2, 7) - 1);
+        const ulong ValidWeeks = 9007199254740991; // (int)(Math.Pow(2, 53) - 1);
+        const ulong ValidMonths = 4095;//  (int)(Math.Pow(2, 12) - 1);
+
         #endregion
 
         #region [ Constructor ]
@@ -77,7 +162,71 @@ namespace Adapt.DataSources
 
         public async IAsyncEnumerable<IFrame> GetData(List<AdaptSignal> signals, DateTime start, DateTime end)
         {
-            yield break;
+            string token = GenerateAntiForgeryToken();
+           
+            IEnumerable<HistorianAggregatePoint> points;
+
+            using (HttpClientHandler handler = new HttpClientHandler() { UseCookies = true })
+            using (HttpClient client = new HttpClient(handler))
+            {
+                try
+                {
+                    client.BaseAddress = new Uri(m_settings.Server);
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    client.DefaultRequestHeaders.Add("X-GSF-Verify", token);
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes($"{m_settings.User}:{m_settings.Password}")));
+
+                    Post post = new Post()
+                    {
+                        Instance = m_settings.Instance,
+                        Aggregate = "*",
+                        Devices = new string[] { },
+                        Phases = new string[] { },
+                        Types = new string[] { },
+                        Hours = ValidHours,
+                        Days = ValidDays,
+                        Weeks = ValidWeeks,
+                        Months = ValidMonths,
+                        StartTime = start,
+                        EndTime = end,
+                        Tags = signals.Select(s => s.ID).ToArray()
+                    };
+                    
+                    HttpResponseMessage response = client.PostAsync($"api/trendap/Query", JsonContent.Create(post)).Result;
+
+                    Task<string> rsp = response.Content.ReadAsStringAsync();
+
+                    points = JsonConvert.DeserializeObject<IEnumerable<HistorianAggregatePoint>>(rsp.Result);
+                }
+                catch (Exception ex)
+                {
+                    yield break;
+                }
+
+                foreach (IGrouping<string,HistorianAggregatePoint> grp in points.GroupBy(item => item.Timestamp))
+                {
+
+                    DateTime TS = DateTime.Parse(grp.Key).ToUniversalTime();
+                    Dictionary<string, ITimeSeriesValue> data = new Dictionary<string, ITimeSeriesValue>();
+                    foreach (HistorianAggregatePoint pt in grp)
+                    {
+                        if (signals.FindIndex(s => s.ID == (pt.Tag)) > -1)
+                            data.Add(pt.Tag,new AdaptValue(pt.Tag, pt.Average, TS));
+                    }
+                    IFrame frame = new Frame()
+                    {
+                        Published = true,
+                        Timestamp = TS,
+                        Measurements = new ConcurrentDictionary<string, ITimeSeriesValue>(data)
+                    };
+
+
+                    yield return frame;
+                }
+                yield break;
+            }
+            
         }
 
       
@@ -186,7 +335,7 @@ namespace Adapt.DataSources
                         name = measurement["SignalReference"].ToString();
 
                     m_Signals.Add(
-                        new AdaptSignal(measurement["SignalID"].ToString(), name, m_Devices.Last())
+                        new AdaptSignal(measurement["ID"].ToString(), name, m_Devices.Last())
                         { 
                             FramesPerSecond = int.Parse(measurement["FramesPerSecond"].ToString()),
                             Description = measurement["Description"].ToString(),
@@ -274,10 +423,45 @@ namespace Adapt.DataSources
             }
         }
 
-        #endregion
+        /// <summary>
+        /// Generates an AntiForgeryToken when connectiong tot he OH
+        /// </summary>
+        /// <returns></returns>
+        private string GenerateAntiForgeryToken()
+        {
 
-        #region [ static ]
-        
-        #endregion
-    }
+            using (HttpClientHandler handler = new HttpClientHandler())
+            using (HttpClient client = new HttpClient(handler))
+            {
+                handler.ServerCertificateCustomValidationCallback = ServerCertificateCustomValidation;
+
+                    
+                client.BaseAddress = new Uri(m_settings.Server);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes($"{m_settings.User}:{m_settings.Password}")));
+
+
+                HttpResponseMessage response = client.GetAsync($"api/rvht").Result;
+
+                if (!response.IsSuccessStatusCode)
+                    return "";
+
+                Task<string> rsp = response.Content.ReadAsStringAsync();
+                return response.Content.ReadAsStringAsync().Result;
+
+            }
+        }
+
+
+            private bool ServerCertificateCustomValidation(HttpRequestMessage requestMessage, X509Certificate2 certificate, X509Chain chain, SslPolicyErrors sslErrors)
+            {
+                return sslErrors == SslPolicyErrors.None;
+            }
+            #endregion
+
+            #region [ static ]
+
+            #endregion
+        }
 }
