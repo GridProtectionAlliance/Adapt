6efbfa3971f46c23423df8eb8ee3a06e381881ee
Christoph Lackner <clackner@gridprotectionalliance.org>
2021-09-27 16:40:13 -0400

    Updated Task Logic

---
+++ Source/Application/Adapt/ViewModels/Task/MappingViewModel.cs
@@ -0,0 +1,213 @@
+﻿// ******************************************************************************************************
+//  MappingViewModel.tsx - Gbtc
+//
+//  Copyright © 2021, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  09/07/2021 - C. Lackner
+//       Generated original version of source code.
+//
+// ******************************************************************************************************
+using Adapt.Models;
+using Adapt.View.Template;
+using Adapt.ViewModels.Common;
+using Gemstone.Data;
+using Gemstone.Data.Model;
+using Gemstone.IO;
+using Gemstone.StringExtensions;
+using GemstoneCommon;
+using GemstoneWPF;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Transactions;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace Adapt.ViewModels
+{
+    /// <summary>
+    /// ViewModel for Task Window
+    /// </summary>
+    public class MappingVM : ViewModelBase
+    {
+        #region [ Members ]
+
+        private Template m_Template;
+        public class Mapping
+        {
+            public Mapping(TemplateInputDevice device, MappingVM parent)
+            {
+                TargetDeviceID = device.ID;
+                TargetDeviceName = device.Name;
+                IsValid = true;
+                ChangeDevice = new RelayCommand(() => parent.SelectDevice(this), () => true);
+                FixChannel = new RelayCommand(() => parent.FixDuplicates(this), () => true);
+
+                ChannelMappings = new Dictionary<int, string>();
+            }
+            public int TargetDeviceID { get; set; }
+            public string SourceDeviceID { get; set; }
+            public string TargetDeviceName { get; set; }
+            public string SourceDeviceName { get; set; }
+            public ICommand ChangeDevice { get; }
+            public ICommand FixChannel { get; }
+            public Dictionary<int,string> ChannelMappings { get; set; }
+            public bool IsValid { get; set; }
+        }
+
+        #endregion
+
+        #region[ Properties ]
+
+        
+        /// <summary>
+        /// The index of the DataSource Selected.
+        /// </summary>
+        public IDataSource DataSource 
+        { 
+            get;
+            set;
+        }
+
+        public DataSource DataSourceModel { get; set; }
+        public ObservableCollection<Mapping> DeviceMappings { get; set; }
+
+        #endregion
+
+        #region [ Constructor ]
+
+        /// <summary>
+        /// Creates a New <see cref="MappingVM"/>
+        /// </summary>
+        /// <param name="template"> The corresponding <see cref="Template"/></param>
+        public MappingVM(Template template)
+        {
+            m_Template = template;
+            LoadTargetDevices();
+        }
+
+        #endregion
+
+        #region [ Methods ]
+        
+        /// <summary>
+        /// Loads the Devices associated with this Template.
+        /// </summary>
+        private void LoadTargetDevices()
+        {
+            DeviceMappings = new ObservableCollection<Mapping>();
+
+            using (AdoDataConnection connection = new AdoDataConnection(ConnectionString, DataProviderString))
+                DeviceMappings = new ObservableCollection<Mapping>(
+                    new TableOperations<TemplateInputDevice>(connection).QueryRecordsWhere("TemplateID = {0}", m_Template.Id)
+                    .Select(d=> new Mapping(d, this)).ToList()
+                    );
+
+            OnPropertyChanged(nameof(DeviceMappings));
+        }
+
+        /// <summary>
+        /// Opens the Select Device Window
+        /// </summary>
+        private void SelectDevice(Mapping mapping)
+        {
+            SelectSignal dateSelection = new SelectSignal();
+            SelectSignalMappingVM<AdaptDevice> dateSelectionVM = new SelectSignalMappingVM<AdaptDevice>((d) => {
+                mapping.SourceDeviceID = d.ID;
+                mapping.SourceDeviceName = d.Name;
+
+                mapping.ChannelMappings = new Dictionary<int, string>();
+
+                // Validate Signals on that device
+                List<int> targetSignals;
+                using (AdoDataConnection connection = new AdoDataConnection(ConnectionString, DataProviderString))
+                    targetSignals = new TableOperations<TemplateInputSignal>(connection)
+                        .QueryRecordsWhere("DeviceID = {0}", mapping.TargetDeviceID).Select(s => s.ID).ToList();
+                    
+                List<AdaptSignal> sourceSignals = AdaptSignal.Get(DataSource, DataSourceModel.ID, ConnectionString, DataProviderString)
+                    .Where(s => s.Device == d.ID).ToList();
+
+                mapping.IsValid = sourceSignals.Count() >= targetSignals.Count();
+                for (int i = 0; i < targetSignals.Count(); i++)
+                    if (i < sourceSignals.Count())
+                        mapping.ChannelMappings.Add(targetSignals[i], sourceSignals[i].ID);
+                    else
+                        mapping.ChannelMappings.Add(targetSignals[i],"");
+                DeviceMappings = new ObservableCollection<Mapping>(DeviceMappings);
+                OnPropertyChanged(nameof(DeviceMappings));
+            },(d,s) => d.Name.ToLower().Contains(s.ToLower()),(d) => d.Name,AdaptDevice.Get(DataSource,DataSourceModel.ID,ConnectionString,DataProviderString));
+            dateSelection.DataContext = dateSelectionVM;
+            dateSelection.Show();
+        }
+
+        /// <summary>
+        /// Opens Window to select signals from a device.
+        /// </summary>
+        public void FixDuplicates(Mapping mapping)
+        {
+            if (mapping.ChannelMappings.Count() == 0 || !mapping.ChannelMappings.ContainsValue(""))
+                return;
+
+            int targetId = mapping.ChannelMappings.First(item => item.Value == "").Key;
+            string title = "Select a Signal for {0}";
+            using (AdoDataConnection connection = new AdoDataConnection(ConnectionString, DataProviderString))
+                title += new TableOperations<TemplateInputSignal>(connection)
+                    .QueryRecordWhere("Id = {0}", targetId)?.Name ?? "";
+
+            SelectSignal signalSelection = new SelectSignal();
+            SelectSignalMappingVM<AdaptSignal> dateSelectionVM = new SelectSignalMappingVM<AdaptSignal>((d) => {
+                mapping.ChannelMappings[targetId] = d.ID;
+                mapping.IsValid = !mapping.ChannelMappings.ContainsValue("");
+                DeviceMappings = new ObservableCollection<Mapping>(DeviceMappings);
+                OnPropertyChanged(nameof(DeviceMappings));
+            }, (d, s) => d.Name.ToLower().Contains(s.ToLower()), (d) => d.Name, AdaptSignal.Get(DataSource, DataSourceModel.ID, ConnectionString, DataProviderString)
+            .Where(s => s.ID == mapping.SourceDeviceID), title);
+            signalSelection.DataContext = dateSelectionVM;
+            signalSelection.Show();
+            
+        }
+        /// <summary>
+        /// Updates the DataSource model and instance
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <param name="model"></param>
+        public void UpdateDataSource(IDataSource instance, DataSource model)
+        {
+            DataSource = instance;
+            DataSourceModel = model;
+
+            OnPropertyChanged(nameof(DataSource));
+            OnPropertyChanged(nameof(DataSourceModel));
+        }
+
+        
+        #endregion
+
+        #region [ Static ]
+
+        private static readonly string ConnectionString = $"Data Source={FilePath.GetAbsolutePath("") + Path.DirectorySeparatorChar}DataBase.db; Version=3; Foreign Keys=True; FailIfMissing=True";
+        private static readonly string DataProviderString = "AssemblyName={System.Data.SQLite, Version=1.0.109.0, Culture=neutral, PublicKeyToken=db937bc2d44ff139}; ConnectionType=System.Data.SQLite.SQLiteConnection; AdapterType=System.Data.SQLite.SQLiteDataAdapter";
+        
+        #endregion
+    }
+}
---
+++ Source/Application/Adapt/ViewModels/Task/TaskViewModel.cs
@@ -0,0 +1,187 @@
+﻿// ******************************************************************************************************
+//  TaskViewModel.tsx - Gbtc
+//
+//  Copyright © 2021, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  09/07/2021 - C. Lackner
+//       Generated original version of source code.
+//
+// ******************************************************************************************************
+using Adapt.Models;
+using Adapt.View.Template;
+using Adapt.ViewModels.Common;
+using Gemstone.Data;
+using Gemstone.Data.Model;
+using Gemstone.IO;
+using Gemstone.StringExtensions;
+using GemstoneCommon;
+using GemstoneWPF;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Transactions;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace Adapt.ViewModels
+{
+    /// <summary>
+    /// ViewModel for Task Window
+    /// </summary>
+    public class TaskVM : ViewModelBase
+    {
+        #region [ Members ]
+        private int m_SelectedTemplateIndex;
+        private int m_SelectedDataSourceIndex;
+        #endregion
+
+        #region[ Properties ]
+
+        /// <summary>
+        /// Represents a List of all available DataSources.
+        /// </summary>
+        public ObservableCollection<DataSource> DataSources { get; set; }
+
+        /// <summary>
+        /// Represents a List of all available Templates.
+        /// </summary>
+        public ObservableCollection<Template> Templates { get; set; }
+        public DateSelectVM TimeSelectionViewModel { get; }
+
+        /// <summary>
+        /// The index of the DataSource Selected.
+        /// </summary>
+        public int SelectedDataSourceIndex { 
+            get => m_SelectedDataSourceIndex; 
+            set
+            {
+                m_SelectedDataSourceIndex = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// The index of the Template Selected.
+        /// </summary>
+        public int SelectedTemplateIndex
+        { 
+            get => m_SelectedTemplateIndex;
+            set
+            {
+                m_SelectedTemplateIndex = value;
+                MappingViewModel = new MappingVM(Templates[m_SelectedTemplateIndex]);
+                ValidateDataSource();
+                
+                OnPropertyChanged();
+            }
+        }
+        //public ObservableCollection<SectionVM> Sections => m_Sections;
+
+        /// <summary>
+        /// The Viewmodel containing the mapping between Template Devices and DataSourceDevices
+        /// </summary>
+        public MappingVM MappingViewModel { get; set; }
+        #endregion
+
+        #region [ Constructor ]
+
+        // #ToDo: Add logic to save Task to temporary File to avoid having to set it up every time Adapt/SciSync is opened
+        public TaskVM()
+        {
+            
+            LoadDataSources();
+            SelectedDataSourceIndex = 0;
+
+            LoadTemplates();
+            m_SelectedTemplateIndex = 0;
+            TimeSelectionViewModel = new DateSelectVM();
+
+            MappingViewModel = new MappingVM(Templates[SelectedTemplateIndex]);
+            ValidateDataSource();
+        }
+
+        #endregion
+
+        #region [ Methods ]
+        
+        /// <summary>
+        /// Loads all Templates available.
+        /// </summary>
+        private void LoadTemplates()
+        {
+            Templates = new ObservableCollection<Template>();
+
+            using (AdoDataConnection connection = new AdoDataConnection(ConnectionString, DataProviderString))
+                Templates = new ObservableCollection<Template>(
+                    new TableOperations<Template>(connection).QueryRecords().ToList()
+                    );
+
+            OnPropertyChanged(nameof(Templates));
+        }
+
+        /// <summary>
+        /// Loads all DataSources available.
+        /// </summary>
+        private void LoadDataSources()
+        {
+            DataSources = new ObservableCollection<DataSource>();
+
+            using (AdoDataConnection connection = new AdoDataConnection(ConnectionString, DataProviderString))
+                DataSources = new ObservableCollection<DataSource>(
+                    new TableOperations<DataSource>(connection).QueryRecords().ToList()
+                    );
+            
+            OnPropertyChanged(nameof(DataSources));
+        }
+
+        private void ValidateDataSource()
+        {
+            try 
+            {
+                IDataSource Instance = (IDataSource)Activator.CreateInstance(DataSources[m_SelectedDataSourceIndex].AssemblyName,DataSources[m_SelectedDataSourceIndex].TypeName).Unwrap();
+                IConfiguration config = new ConfigurationBuilder().AddGemstoneConnectionString(DataSources[m_SelectedDataSourceIndex].ConnectionString).Build();
+                Instance.Configure(config);
+
+                if (MappingViewModel != null)
+                    MappingViewModel.UpdateDataSource(Instance, DataSources[m_SelectedDataSourceIndex]);
+
+                OnPropertyChanged(nameof(MappingVM));
+            }
+            catch (Exception ex)
+            {
+                Popup($"The Datasource could not be loaded: {ex.Message}","An Error Occurred", MessageBoxImage.Error);
+
+            }
+        }
+        #endregion
+
+        #region [ Static ]
+
+        private static readonly string ConnectionString = $"Data Source={FilePath.GetAbsolutePath("") + Path.DirectorySeparatorChar}DataBase.db; Version=3; Foreign Keys=True; FailIfMissing=True";
+        private static readonly string DataProviderString = "AssemblyName={System.Data.SQLite, Version=1.0.109.0, Culture=neutral, PublicKeyToken=db937bc2d44ff139}; ConnectionType=System.Data.SQLite.SQLiteConnection; AdapterType=System.Data.SQLite.SQLiteDataAdapter";
+        
+        #endregion
+    }
+}
