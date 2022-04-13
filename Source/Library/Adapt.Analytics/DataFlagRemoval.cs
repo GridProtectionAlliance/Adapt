956e2b8dc765e9b2739624564a6ef6ba5a4a0d26
Christoph Lackner <clackner@gridprotectionalliance.org>
2022-04-13 16:24:45 -0400

    Added Data Flag Removal for Bad Data Flag

---
+++ Source/Library/Adapt.Analytics/DataFlagRemoval.cs
@@ -0,0 +1,80 @@
+﻿// ******************************************************************************************************
+//  DataFlagRemoval.tsx - Gbtc
+//
+//  Copyright © 2021, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  08/02/2021 - C. Lackner
+//       Generated original version of source code.
+//  12/16/2021 - A. Hagemeyer
+//       Changed to a zeros analytic
+//
+// ******************************************************************************************************
+
+
+using Adapt.Models;
+using GemstoneCommon;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Adapt.DataSources
+{
+    /// <summary>
+    /// If value = 0 return NaN. Otherwise return the data
+    /// </summary>
+    
+    [AnalyticSection(AnalyticSection.DataCleanup)]
+    [Description("Data Flag: Removes values from the zero that have the Flag Set.")]
+    public class DataFlagRemoval : BaseAnalytic, IAnalytic
+    {
+        private Setting m_settings;
+        public class Setting { }
+
+        public Type SettingType => typeof(Setting);
+
+
+        public IEnumerable<AnalyticOutputDescriptor> Outputs()
+        {
+            return new List<AnalyticOutputDescriptor>() { 
+                new AnalyticOutputDescriptor() { Name = "Cleaned", FramesPerSecond = 0, Phase = Phase.NONE, Type = MeasurementType.Other } 
+            };
+        }
+
+        public IEnumerable<string> InputNames()
+        {
+            return new List<string>() { "Original", "Flag" };
+        }
+
+        public override ITimeSeriesValue[] Compute(IFrame frame, IFrame[] prev, IFrame[] future) 
+        {
+            ITimeSeriesValue original = frame.Measurements["Original"];
+            ITimeSeriesValue flag = frame.Measurements["Flag"];
+            if (!double.IsNaN(flag.Value) && flag.Value > 0.0D)
+                return new AdaptValue[] { new AdaptValue("Cleaned", double.NaN, frame.Timestamp) };
+            else
+                return new AdaptValue[] { new AdaptValue("Cleaned", original.Value, frame.Timestamp) };
+        }
+
+        public void Configure(IConfiguration config)
+        {
+            m_settings = new Setting();
+            config.Bind(m_settings);
+        }
+    }
+}
